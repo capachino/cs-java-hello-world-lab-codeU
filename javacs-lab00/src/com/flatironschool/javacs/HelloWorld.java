ddb344c51be4fdec130ff4c2fd218656c93e6020
jyum <jyum@localhost>
2016-06-28 17:16:42 -0700

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,6 +2,7 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
+        return Double.parseDouble(System.getProperty("java.specification.version"));
         // I won't work!
     }
 
